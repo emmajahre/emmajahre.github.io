1edc82b29f33ccd2c28f14c320502bf5e566a8fb
Emma Jahre <emmajahre@live.no>
2019-04-30 15:13:55 +0200

    c

--- vår-prosjekt2/produktdetalj.js
+++ vår-prosjekt2/produktdetalj.js
@@ -1,32 +1,33 @@
-let bildecontainer = document.querySelector("#bildecontainer")
-let sykdom = document.querySelector("#sykdom")
+const main = document.querySelector("main")
 
 var url_string = window.location.href;
 var url = new URL (url_string);
 var id = url.searchParams.get("id");
 
-let db = firebase.database();
-let sykdomhund = db.ref("Dyr/Hunder/" + id);
-let sykdomkatt = db.ref("Dyr/Katter/" + id);
+const db = firebase.database();
+const infoGenser = db.ref("varer/genserer/" + id);
+const infoTopper = db.ref("varer/topper/" + id);
+const infoBukser = db.ref("varer/bukser/" + id);
+const infoSkjort = db.ref("varer/skjørt/" + id);
+const infoKjoler = db.ref("varer/kjoler/" + id);
 
 function genererHTML(snapshot){
-  let dyr = snapshot.val();
-  console.log(snapshot.val())
+  let varer = snapshot.val();
+  main.innerHTML = `
+    <p>${varer.navn}</p>
 
-  bildecontainer.innerHTML = `
-    <img src=$(dyr.Bilde)>
   `;
 
-  sykdom.innerHTML=`
-  <p>Navn: $(dyr.Navn)</p>
-  <p>Journal: $(dyr.sykdom)</p>
-  `;
 }
 
-if (id.charAt(0) === "H"){
-  sykdomhund.on("value", genererHTML);
-}
-
-else if(id.charAt(0) === "K"){
-  sykdomkatt.on("value",genererHTML);
-}
+if (id.charAt(0) === "g") {
+    infoGenser.on("value",genererHTML)
+  } else if (id.charAt(0) === "t") {
+    infoTopper.on("value",genererHTMLL)
+  } else if (id.charAt(0) === "b") {
+    infoBukser.on("value",genererHTMLLL)
+  } else if (id.charAt(0) === "s") {
+    infoSkjort.on("value",genererHTMLLLL)
+  } else if (id.charAt(0) === "k") {
+    infoKjoler.on("value",genererHTMLLLL)
+  }
